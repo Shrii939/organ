19f9877c0931a851da7cc65ff620aba5e8988586
Shrii939 <shridhars145@gmail.com>
2023-01-20 13:12:27 +0530

    databse

--- organn.sql
+++ organn.sql
@@ -3,11 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
-<<<<<<< HEAD
--- Generation Time: Jan 19, 2023 at 07:17 PM
-=======
--- Generation Time: Jan 19, 2023 at 07:50 PM
->>>>>>> cd6f2b0 (added functionality)
+-- Generation Time: Jan 20, 2023 at 08:41 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -66,24 +62,15 @@ CREATE TABLE `orders` (
 
 INSERT INTO `orders` (`Hname`, `Haddr`, `contact`, `dt`, `Oid`) VALUES
 ('my hst', 'asdf', 1234, '2023-01-19', 2),
-<<<<<<< HEAD
-('my hst', 'asdf', 1234, '2023-01-19', 3);
-=======
 ('my hst', 'asdf', 1234, '2023-01-19', 3),
 ('aj', 'banglore', 1234456, '2023-01-17', 5);
->>>>>>> cd6f2b0 (added functionality)
 
 --
 -- Triggers `orders`
 --
 DELIMITER $$
-<<<<<<< HEAD
-CREATE TRIGGER `make_history` BEFORE INSERT ON `orders` FOR EACH ROW INSERT INTO triggerA (Hname, Oid, Did, Otype, Odetails)
-   VALUES((SELECT Hname from orders), (SELECT Oid, Did, Otype, Odetails FROM organ))
-=======
 CREATE TRIGGER `make_history` BEFORE INSERT ON `orders` FOR EACH ROW INSERT INTO triggerA
 SELECT * FROM organ
->>>>>>> cd6f2b0 (added functionality)
 $$
 DELIMITER ;
 
@@ -100,6 +87,13 @@ CREATE TABLE `organ` (
   `Odetails` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `organ`
+--
+
+INSERT INTO `organ` (`Oid`, `Did`, `Otype`, `Odetails`) VALUES
+(7, 1, 'eye', 'ha');
+
 -- --------------------------------------------------------
 
 --
@@ -113,6 +107,13 @@ CREATE TABLE `organ_bank` (
   `oissue` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `organ_bank`
+--
+
+INSERT INTO `organ_bank` (`Oid`, `tdetails`, `Otype`, `oissue`) VALUES
+(7, 'rejected', 'eye', 'was fine meri marzi');
+
 -- --------------------------------------------------------
 
 --
@@ -120,10 +121,6 @@ CREATE TABLE `organ_bank` (
 --
 
 CREATE TABLE `triggerA` (
-<<<<<<< HEAD
-  `Hname` varchar(30) DEFAULT NULL,
-=======
->>>>>>> cd6f2b0 (added functionality)
   `Oid` int(11) NOT NULL,
   `Did` int(11) NOT NULL,
   `Otype` varchar(20) NOT NULL,
@@ -136,6 +133,27 @@ CREATE TABLE `triggerA` (
 
 INSERT INTO `triggerA` (`Oid`, `Did`, `Otype`, `Odetails`) VALUES
 (5, 1, 'heart', 'healthy lovable heart');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(11) NOT NULL,
+  `password` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(2, '20cs082', '1234'),
+(3, '', '1234'),
+(4, '', '');
 
 --
 -- Indexes for dumped tables
@@ -167,6 +185,12 @@ ALTER TABLE `organ_bank`
   ADD PRIMARY KEY (`Oid`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -175,6 +199,12 @@ ALTER TABLE `organ_bank`
 --
 ALTER TABLE `donor`
   MODIFY `Did` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
